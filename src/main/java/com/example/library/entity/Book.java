e510a2a531a1de9bcc39e5645887997a8782526d
villiger9 <ali.shaker93@gmail.com>
2024-01-28 20:32:01 +0300

    added getters and setters for book class

--- src/main/java/com/example/library/entity/Book.java
+++ src/main/java/com/example/library/entity/Book.java
@@ -22,4 +22,36 @@ public class Book {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
